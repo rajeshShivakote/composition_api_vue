5ba5319708570e742acf63f1c61074b36ff0aa39
rajeshshivakote <rajeshs.shivarudraiah@gmail.com>
2022-03-09 16:13:06 +0530

    methods

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,63 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
+    <button @click="setAge">Submit</button>
+    <div>
+      <input type="text" placeholder="First Name"/>
+      <input type="text" placeholder="Last Name"/>
+    </div>
   </section>
 </template>
 
 <script>
+// import { ref } from 'vue';
+import { reactive } from 'vue';
+
 export default {
-  data() {
-    return {
-      userName: 'Maximilian',
-    };
-  },
+  setup() {
+      // const uName = ref('Maximillan');
+      // const uAge = ref(31);
+      // setTimeout(function() {
+      //   uName.value = 'Max';
+      // },2000)
+      // return { userName: uName, age: uAge}
+    //  const uAge = ref(31);
+      const user = reactive({
+        name: 'Maxmillan',
+        age: 31
+      })
+
+      function setNewAge() {
+        user.age = 20;
+      }
+      // console.log(isRef(uAge.value));  // false
+      // console.log(isReactive(user.name)); // false
+
+      // console.log(isRef(uAge));  // True
+      // console.log(isReactive(user)); // True
+
+      // const userRefs = toRefs(user);
+      // console.log(isRef(userRefs.name)); // true (convert reactive to refs)
+
+      // setTimeout(function(){
+      //   user.name = "Max",
+      //   user.age = 34
+      // },2000)
+
+      return { user: user, setAge: setNewAge}
+  }
+  // data() {
+  //   return {
+  //     userName: 'Maximilian',
+  //     age: 32
+  //   };
+  // },
+  // methods: {
+  //   setNewAge() {
+  //     this.age = 32
+  //   }
+  // },
 };
 </script>
 
